36487e5db7b7622553c78708bd72dfbb9c17238a
abrilfernandez <109990436+abrilfernandez@users.noreply.github.com>
2023-09-18 13:49:12 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+alert ("bienvenidos a la escuela nuestra senora de lourdes"); 
+let parcial, final, promedio;
+ parcial=prompt( "ingresar parcial");
+ final=prompt( "ingresar final");
+let resultado= (parcial+final)/2;
+    if(resultado >= 100){
+        alert("el promedio es" +resultado+"APROBADO");
+     }
+      else{
+          alert("el promedio es" + resultado+ "DESAPROBADO"); 
+       } 
+       
